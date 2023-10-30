7425f8da2c05482d04a5b4d1f0a0773519899da2
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2023-10-30 22:01:02 +0100

    Add curlHandle to exception

--- src/Exception/RequestException.php
+++ src/Exception/RequestException.php
@@ -10,19 +10,28 @@ class RequestException extends \RuntimeException
      * @var string
      */
     private $response;
+    private ?\CurlHandle $curlHandle;
 
     public function __construct(
         string $message = "",
         int $code = 0,
         string $response = '',
+        ?\CurlHandle $curlHandle = null,
         ?\Throwable $previous = null
     ) {
         parent::__construct($message, $code, $previous);
         $this->response = $response;
+        $this->curlHandle = $curlHandle;
     }
 
     public function getResponse(): string
     {
         return $this->response;
     }
+
+    public function curlHandle(): \CurlHandle
+    {
+        return $this->curlHandle;
+    }
+
 }
